e7c9e7d0205b550b5eace3f307fee4e296366adc
Nick Hart <nhartmusic@gmail.com>
2020-07-22 13:21:52 +0200

    bugs fixed and changed from 0 to -1

--- polynomial.js
+++ polynomial.js
@@ -1,15 +1,16 @@
 let x_vals = []
 let y_vals = []
 
-let m, b
+let a, b, c
 
 const learningRate = 0.5
 const optimizer = tf.train.sgd(learningRate)
 
 function setup() {
     createCanvas(400, 400)
-    m = tf.variable(tf.scalar(random(1)))
+    a = tf.variable(tf.scalar(random(1)))
     b = tf.variable(tf.scalar(random(1)))
+    c = tf.variable(tf.scalar(random(1)))
 }
 
 function loss(pred, labels) {
@@ -27,8 +28,8 @@ function predict(x) {
 }
 
 function mousePressed() {
-    let x = map(mouseX, 0, width, -1, 1)
-    let y = map(mouseY, 0, height, 1, -1)
+    let x = map(mouseX, 0, width, 0, 1)
+    let y = map(mouseY, 0, height, 1, 0)
     x_vals.push(x)
     y_vals.push(y)
 }
@@ -46,22 +47,22 @@ function draw() {
     stroke(255)
     strokeWeight(8)
     for (let i = 0; i < x_vals.length; i++) {
-        let px = map(x_vals[i], -1, 1, 0, width)
-        let py = map(y_vals[i], -1, 1, height, 0)
+        let px = map(x_vals[i], 0, 1, 0, width)
+        let py = map(y_vals[i], 0, 1, height, 0)
         point(px, py)
     }
 
-    const lineX = [-1, 1]
+    const lineX = [0, 1]
 
     const ys = tf.tidy(() => predict(lineX))
     let lineY = ys.dataSync()
     ys.dispose()
 
-    let x1 = map(lineX[0], -1, 1, 0, width)
-    let x2 = map(lineX[1], -1, 1, 0, width)
+    let x1 = map(lineX[0], 0, 1, 0, width)
+    let x2 = map(lineX[1], 0, 1, 0, width)
 
-    let y1 = map(lineY[0], -1, 1, height, 0)
-    let y2 = map(lineY[1], -1, 1, height, 0)
+    let y1 = map(lineY[0], 0, 1, height, 0)
+    let y2 = map(lineY[1], 0, 1, height, 0)
 
     strokeWeight(2)
     line(x1, y1, x2, y2)
