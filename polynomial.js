e3cc3c5bd4f956bfc018d98bd807104dcc0bae50
Nick Hart <nhartmusic@gmail.com>
2020-07-22 15:35:27 +0200

    successfully modeling polynomial regression now

--- polynomial.js
+++ polynomial.js
@@ -1,16 +1,18 @@
 let x_vals = []
 let y_vals = []
 
-let a, b, c
+let a, b, c, d
+let dragging = false
 
-const learningRate = 0.5
-const optimizer = tf.train.sgd(learningRate)
+const learningRate = 0.05
+const optimizer = tf.train.adam(learningRate)
 
 function setup() {
     createCanvas(400, 400)
-    a = tf.variable(tf.scalar(random(1)))
-    b = tf.variable(tf.scalar(random(1)))
-    c = tf.variable(tf.scalar(random(1)))
+    a = tf.variable(tf.scalar(random(-1, 1)))
+    b = tf.variable(tf.scalar(random(-1, 1)))
+    c = tf.variable(tf.scalar(random(-1, 1)))
+    d = tf.variable(tf.scalar(random(-1, 1)))
 }
 
 function loss(pred, labels) {
@@ -22,50 +24,70 @@ function loss(pred, labels) {
 
 function predict(x) {
     const xs = tf.tensor1d(x)
-    // y = mx + b
-    const ys = xs.mul(m).add(b)
+    // y = ax^3 + bx^2 + cx + d
+    const ys = xs.pow(tf.scalar(3)).mul(a)
+        .add(xs.square().mul(b))
+        .add(xs.mul(c))
+        .add(d)
     return ys
 }
 
 function mousePressed() {
-    let x = map(mouseX, 0, width, 0, 1)
-    let y = map(mouseY, 0, height, 1, 0)
-    x_vals.push(x)
-    y_vals.push(y)
+    dragging = true
 }
 
+function mouseReleased() {
+    dragging = false
+}
+
+// function mouseDragged() {
+
+// }
+
 function draw() {
-    tf.tidy(() => {
-        if (x_vals.length > 0) {
-            const ys = tf.tensor1d(y_vals)
-            optimizer.minimize(() => loss(predict(x_vals), ys))
-        }
-    })
+    if (dragging) {
+        let x = map(mouseX, 0, width, -1, 1)
+        let y = map(mouseY, 0, height, 1, -1)
+        x_vals.push(x)
+        y_vals.push(y)
+    } else {
+        tf.tidy(() => {
+            if (x_vals.length > 0) {
+                const ys = tf.tensor1d(y_vals)
+                optimizer.minimize(() => loss(predict(x_vals), ys))
+            }
+        })
+    }
 
     background(0)
 
     stroke(255)
     strokeWeight(8)
     for (let i = 0; i < x_vals.length; i++) {
-        let px = map(x_vals[i], 0, 1, 0, width)
-        let py = map(y_vals[i], 0, 1, height, 0)
+        let px = map(x_vals[i], -1, 1, 0, width)
+        let py = map(y_vals[i], -1, 1, height, 0)
         point(px, py)
     }
 
-    const lineX = [0, 1]
+    const curveX = []
+    for (let x = -1; x < 1; x += 0.05) {
+        curveX.push(x)
+    }
 
-    const ys = tf.tidy(() => predict(lineX))
-    let lineY = ys.dataSync()
+    const ys = tf.tidy(() => predict(curveX))
+    let curveY = ys.dataSync()
     ys.dispose()
 
-    let x1 = map(lineX[0], 0, 1, 0, width)
-    let x2 = map(lineX[1], 0, 1, 0, width)
-
-    let y1 = map(lineY[0], 0, 1, height, 0)
-    let y2 = map(lineY[1], 0, 1, height, 0)
-
+    beginShape()
+    noFill()
+    stroke(255)
     strokeWeight(2)
-    line(x1, y1, x2, y2)
+    for (let i = 0; i < curveX.length; i++) {
+        let x = map(curveX[i], -1, 1, 0, width)
+        let y = map(curveY[i], -1, 1, height, 0)
+        vertex(x, y)
+    }
+    endShape()
 
     console.log(tf.memory().numTensors)
 }
